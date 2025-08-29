7a48bfea9025afa4edbde20c8a1bf61e3155c340
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-08-29 13:59:03 +0200

    Update

--- Src/OpenGrokWebApiShare/OpenGrok.cs
+++ Src/OpenGrokWebApiShare/OpenGrok.cs
@@ -1,8 +1,10 @@
-﻿namespace OpenGrokWebApi;
+﻿using System.Text.RegularExpressions;
+
+namespace OpenGrokWebApi;
 
 // https://opengrok.docs.apiary.io/#reference/0/web-app-version/get-web-app-version-as-string?console=1
 
-public class OpenGrok : JsonService
+public partial class OpenGrok : JsonService
 {
     public OpenGrok(string storeKey, string appName) : base(storeKey, appName, SourceGenerationContext.Default)
     { }
@@ -13,24 +15,25 @@ public class OpenGrok : JsonService
     //protected override string? AuthenticationTestUrl => "system/ping";
 
 
+    // https://shm-opengrok.elektrobit.com/source/api/v1/suggest/config
+
+    // curl -H "Authorization: Basic YnM6VmlzdWFsRW50ZTYuMVNwMg==" https://shm-opengrok.elektrobit.com/source/
+
+
     public override async Task<string?> GetVersionStringAsync(CancellationToken cancellationToken = default)
     {
         WebServiceException.ThrowIfNotConnected(client);
 
-        //var res0 = await GetStringAsync("system", cancellationToken);
-        try
-        {
-            var res = await GetStringAsync("api/v1/system/ping", cancellationToken);
-        }
-        catch (Exception) { }
+        var html = await GetStringAsync("", cancellationToken);
 
-        try
-        {
-            var res = await GetStringAsync("api/v1/system/version", cancellationToken);
-        }
-        catch (Exception) { }
+        if (string.IsNullOrWhiteSpace(html)) return null;
 
-       
-        return "0.0.0";
+        
+        var match = VersionRegex().Match(html);
+        string s = match.Success ? match.Groups[1].Value : "0.0.0";
+        return s;
     }
+
+    [GeneratedRegex(@"<meta\s+name=[""']generator[""'][^>]*content=[""']\{?OpenGrok\s+([0-9.]+)", RegexOptions.IgnoreCase, "de-DE")]
+    private static partial Regex VersionRegex();
 }
